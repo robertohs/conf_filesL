6e95cedcf781f08ec060b4895f23d5c4990d365e
robert f <theruph@gmail.com>
2025-10-23 04:52:10 -0700

    auto save

--- scripts/save_configs.sh
+++ scripts/save_configs.sh
@@ -14,35 +14,35 @@ cp -r "$SOURCE_DIR" "$DEST_DIR"
 echo -e "       $NAME  ...  🟢 ok "
 }
 
-if [ ! -d "$DIR/conf_n" ]; then
-  mkdir "$DIR/conf_n"
-  echo "Directory '$DIR/conf_n' created."
+if [ ! -d "$DIR/conf_filesL" ]; then
+  mkdir "$DIR/conf_filesL"
+  echo "Directory '$DIR/conf_filesL' created."
 else
   echo "Directory seems right."
 fi
 
 NAME="scripts"
 SOURCE_DIR="$HOME/scripts"
-DEST_DIR="$DIR/conf_n/scripts"
+DEST_DIR="$DIR/conf_filesL/scripts"
 doThing
 
 NAME="bashrc"
 SOURCE_DIR="$HOME/.bashrc"
-DEST_DIR="$DIR/conf_n/.bashrc"
+DEST_DIR="$DIR/conf_filesL/.bashrc"
 doThing
 NAME="nvim"
 SOURCE_DIR="$HOME/.config/nvim"
-DEST_DIR="$DIR/conf_n/nvim/"
+DEST_DIR="$DIR/conf_filesL/nvim/"
 doThing
 
 NAME="hyprland conf .conf/hypr "
 SOURCE_DIR="$HOME/.config/hypr"
-DEST_DIR="$DIR/conf_n/hypr"
+DEST_DIR="$DIR/conf_filesL/hypr"
 doThing
 
 NAME="matte black hypr"
 SOURCE_DIR="$HOME/.local/share/omarchy/themes/matte-black"
-DEST_DIR="$DIR/conf_n/matte-black"
+DEST_DIR="$DIR/conf_filesL/matte-black"
 doThing
 
 NAME="aple"
@@ -52,7 +52,7 @@ doThing
 
 NAME="waybar"
 SOURCE_DIR="$HOME/.config/waybar"
-DEST_DIR="$DIR/conf_n/waybar"
+DEST_DIR="$DIR/conf_filesL/waybar"
 doThing
 
 
@@ -65,7 +65,7 @@ echo -e "      \n       diff \n\n"
 
 read -n 1 -s -r -p "Press any key to continue..."
 
-cd $DIR/conf_n
+cd $DIR/conf_filesL
 git diff
 read -p "git push?  [Yy]yes /[n]no " -n 1 -r
 echo    # (optional) move to a new line
